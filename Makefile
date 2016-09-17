5d10119270efb83f5c223fee63b42eb31a5e1c89
Ricky <codemonkeyricky@gmail.com>
2016-09-17 09:50:50 -0700

    More changes.

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 all :
-	g++-4.9 -pg -g -O3 -march=native main.cpp -std=gnu++11 -o sg
+	g++-4.9 -pg -g -O0 -march=native main.cpp -std=gnu++11 -o sg_debug
+	g++-4.9 -pg -g -O3 -march=native main.cpp -std=gnu++11 -o sg_release
--- main.cpp
+++ main.cpp
@@ -33,92 +33,87 @@ int kBase   = 26;
 static int count = 0;
 
 void vsearch(
-    char    t[ 4096 ],
-    char    m[ 4096 ],
-    int     shash,
-    int     slen,
-    int     p
+    __m128i input[ 1024 ],
+    __m128i output[ 1024 ],
+    int targetHash,
+    int slen,
+    int p
     )
 {
-    int mapSize = 1024;
+    int mapSize = 4096;
     int result[ 4 ] = { 0 }; 
 
-    __m128i zeroes      = _mm_set_epi32( 0, 0, 0, 0 );
-    __m128i ones        = _mm_set_epi32( 1, 1, 1, 1 );
-    __m128i currHash    = _mm_set_epi32( 0, 0, 0, 0 );
-    __m128i targetHash  = _mm_set_epi32( shash, shash, shash, shash );
-    __m128i modMask     = _mm_set_epi32( kMod - 1, kMod - 1, kMod - 1, kMod - 1 );
+    __m128i zeroes          = _mm_set_epi32( 0, 0, 0, 0 );
+    __m128i ones            = _mm_set_epi32( 1, 1, 1, 1 );
+    __m128i currHash        = _mm_set_epi32( 0, 0, 0, 0 );
+    __m128i targetHash128   = _mm_set_epi32( targetHash, targetHash, targetHash, targetHash );
+    __m128i modMask         = _mm_set_epi32( kMod - 1, kMod - 1, kMod - 1, kMod - 1 );
 
-//    __m128i remove;
-    __m128i power       = _mm_set_epi32( p, p, p, p );
-    __m128i base        = _mm_set_epi32( kBase, kBase, kBase, kBase );
-    __m128i mod         = _mm_set_epi32( kMod, kMod, kMod, kMod );
-    volatile __m128i thash;
+    __m128i power128        = _mm_set_epi32( p, p, p, p );
+    __m128i kBase128        = _mm_set_epi32( kBase, kBase, kBase, kBase );
+    __m128i kMod128         = _mm_set_epi32( kMod, kMod, kMod, kMod );
+    __m128i rollingHash128  = zeroes;
 
-    __m128i remove[ slen ];
-
-    int output[ 4 ];
     for ( auto i = 0; i < slen; i++ )
     {
         // thash = ( thash * kBase + t[ i ] ) % kMod;
 
-        int input[ 4 ] = { t[ i * 4 + 0 ], t[ i * 4 + 1 ], t[ i * 4 + 2 ], t[ i * 4 + 3 ] };
+        auto tadd       = input[ i ]; // _mm_load_si128( reinterpret_cast< __m128i *>( &input[ i * 4 ] ) );
 
-        auto tadd   = _mm_load_si128( reinterpret_cast< __m128i *>( &input[ 0 ] ) );
-        remove[ i ] = tadd;
+        auto a          = _mm_mullo_epi32( rollingHash128, kBase128 );
+        auto b          = _mm_add_epi32( a, tadd );
 
-        auto a      = _mm_mullo_epi32( thash, base );
-        auto b      = _mm_add_epi32( a, tadd );
-
-        thash       = _mm_and_si128( b, modMask );
-
-        _mm_store_si128( reinterpret_cast< __m128i *>( &output[ 0 ] ), thash );
+        rollingHash128  = _mm_and_si128( b, modMask );
     }
 
+    vector< int > hashes[ 4 ];
+
+    int index = 0; 
     for ( auto i = slen; i < mapSize / 4; i++ )
     {
         // if ( rollingHash == targetHash ) { }
-        int output[ 4 ];
-        auto result = _mm_cmpeq_epi32( thash, targetHash );
-        _mm_store_si128( reinterpret_cast< __m128i *>( output ), result );
-        m[ i - slen + 0 ] = ( ( char * ) output )[ 0 ];
-        m[ i - slen + 1 ] = ( ( char * ) output )[ 4 ];
-        m[ i - slen + 2 ] = ( ( char * ) output )[ 8 ];
-        m[ i - slen + 3 ] = ( ( char * ) output )[ 12 ];
 
-//        // TODO:
-//        if ( m[ i - slen + 0 ]
-//        || m[ i - slen + 1 ]
-//        || m[ i - slen + 2 ]
-//        || m[ i - slen + 3 ]
-//        )
-//        {
-//            count++;
-//        }
-
-        int input[ 4 ] = { t[ i * 4 + 0 ], t[ i * 4 + 1 ], t[ i * 4 + 2 ], t[ i * 4 + 3 ] };
+        auto result = _mm_cmpeq_epi32( rollingHash128, targetHash128 );
+        output[ i ] = result;
 
         // thash -= ( t[ i - s.size() ] * power ) % kMod;
 
-        auto a      = _mm_mul_epu32( remove[ ( i - slen ) % slen ], power );
-        auto b      = _mm_and_si128( a, modMask );
-        thash       = _mm_sub_epi32( thash, b );
+        // printf( "%c", input[ 1 ] );
+        auto remove         = input[ i - 5 ]; // _mm_load_si128( reinterpret_cast< __m128i *>( &input[ ( i - 5 ) * 4 ] ) );
+        auto a              = _mm_mullo_epi32( remove, power128 );
+        auto b              = _mm_and_si128( a, modMask );
+        rollingHash128      = _mm_sub_epi32( rollingHash128, b );
 
         // if ( thash < 0 ) thash += kMod;
 
-        auto cmp    = _mm_cmplt_epi32( thash, zeroes );
-        auto to_add = _mm_and_si128( mod, cmp );
-        thash       = _mm_add_epi32( thash, to_add );
+        auto cmp            = _mm_cmplt_epi32( rollingHash128, zeroes );
+        auto to_add         = _mm_and_si128( kMod128, cmp );
+        rollingHash128      = _mm_add_epi32( rollingHash128, to_add );
 
         // thash = ( thash * kBase + t[ i ] ) % kMod;
 
-        auto tadd           = _mm_load_si128( reinterpret_cast< __m128i *>( &input[ 0 ] ) );
-        remove[ i % slen ]  = tadd;
+        auto tadd           = input[ i ]; // _mm_load_si128( reinterpret_cast< __m128i *>( &input[ i * 4 ] ) );
 
-        a           = _mm_mul_epu32( thash, base );
-        b           = _mm_add_epi32( a, tadd );
+        a                   = _mm_mullo_epi32( rollingHash128, kBase128 );
+        b                   = _mm_add_epi32( a, tadd );
 
-        thash       = _mm_and_si128( b, modMask );
+        rollingHash128      = _mm_and_si128( b, modMask );
+    }
+
+//    vector< int > total_hashes;
+//    for ( auto k = 0; k < 4; k++ )
+//    {
+//        total_hashes.insert( total_hashes.end(), hashes[ k ].begin(), hashes[ k ].end() );
+//    }
+
+    for ( auto i = 0; i < 4; i++ )
+    {
+        for ( auto & hash : hashes[ i ] )
+        {
+//            printf( "0x%08x\n", hash );
+        }
+
+//        printf( "new set\n" );
     }
 
 //    return result[ 0 ]
@@ -133,7 +128,7 @@ int main()
     int mapSize = 4096;
     int fd      = open( "big.txt", O_RDONLY );
     int size    = lseek( fd, 0, SEEK_END );
-    string s    = "EBook";
+    string s    = "woman";
 
     vector< Match > matches;
 
@@ -154,6 +149,8 @@ int main()
             targetHash  = ( targetHash * kBase + s[ i ] ) % kMod;
         }
 
+//        printf( "target hash : 0x%08x\n", targetHash );
+
 #if 0
         int found = 0;
         for ( auto i = s.size(); i < mapSize; i++ )
@@ -168,6 +165,8 @@ int main()
                 }
             }
 
+//            printf( "0x%08x\n", rollingHash );
+
             rollingHash -= ( t[ i - s.size() ] * power ) % kMod;
             if ( rollingHash < 0 )
             {
@@ -179,21 +178,33 @@ int main()
 #endif
 
 #if 1
-        char remap[ mapSize ];
+        __m128i remap[ mapSize ];
         for ( auto i = 0; i < mapSize / 4; i++ )
         {
-            remap[ i * 4 + 0 ] = t[ 1024 * 0 + i ];
-            remap[ i * 4 + 1 ] = t[ 1024 * 1 + i ];
-            remap[ i * 4 + 2 ] = t[ 1024 * 2 + i ];
-            remap[ i * 4 + 3 ] = t[ 1024 * 3 + i ];
+            int *input = ( int * ) &remap[ i ];
+            input[ 0 ] = t[ 1024 * 0 + i ];
+            input[ 1 ] = t[ 1024 * 1 + i ];
+            input[ 2 ] = t[ 1024 * 2 + i ];
+            input[ 3 ] = t[ 1024 * 3 + i ];
         }
 #endif
 
         char thash_arr[ 4 ] = { 0 };
-        char output[ mapSize ];
+//        int output[ mapSize ];
+        __m128i output[ 1024 ];
         vsearch( remap, output, targetHash, s.size(), power );
 
-        printf( "%c", thash_arr[ 2 ] );
+//        auto temp = output[ 0 ];
+
+        for ( auto i = 0; i < 1024; i++ )
+        {
+            count += ( ( int * ) &output[ i ] )[ 0 ] & 1;
+            count += ( ( int * ) &output[ i ] )[ 1 ] & 1;
+            count += ( ( int * ) &output[ i ] )[ 2 ] & 1;
+            count += ( ( int * ) &output[ i ] )[ 3 ] & 1;
+        }
+
+//        printf( "%c", thash_arr[ 2 ] );
     }
 
     cout << "Matches = " << matches.size() << endl;
