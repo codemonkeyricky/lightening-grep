7213ec4b2b32d7dac93005c6d0dc7ad0c91b7fd3
Ricky <codemonkeyricky@gmail.com>
2016-09-18 09:55:15 -0700

    Various changes.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <vector>
 #include <immintrin.h>
+#include "immintrin.h"
 #include <assert.h>
 
 using namespace std;
@@ -31,110 +32,108 @@ int kBase   = 26;
 #endif
 
 static int count = 0;
+static int lineCount = 0;
+
+#define MMAP_SIZE   ( 4096 * 4 )
 
 void vsearch(
-    __m128i input[ 1024 ],
-    __m128i output[ 1024 ],
+    int     streams,
+    int     input[ MMAP_SIZE ],
+    __m256i output[ 512 ],
     int targetHash,
     int slen,
     int p
     )
 {
-    int mapSize = 4096;
-    int result[ 4 ] = { 0 }; 
+    int streamSize          = MMAP_SIZE / streams;
 
-    __m128i zeroes          = _mm_set_epi32( 0, 0, 0, 0 );
-    __m128i ones            = _mm_set_epi32( 1, 1, 1, 1 );
-    __m128i currHash        = _mm_set_epi32( 0, 0, 0, 0 );
-    __m128i targetHash128   = _mm_set_epi32( targetHash, targetHash, targetHash, targetHash );
-    __m128i modMask         = _mm_set_epi32( kMod - 1, kMod - 1, kMod - 1, kMod - 1 );
+    __m256i zeroes          = _mm256_set_epi32( 0, 0, 0, 0, 0, 0, 0, 0 );
+    __m256i currHash        = zeroes;
+    __m256i targetHash128   = _mm256_set_epi32( targetHash, targetHash, targetHash, targetHash, targetHash, targetHash, targetHash, targetHash );
+    __m256i modMask         = _mm256_set_epi32( kMod - 1, kMod - 1, kMod - 1, kMod - 1, kMod - 1, kMod - 1, kMod - 1, kMod - 1 );
 
-    __m128i power128        = _mm_set_epi32( p, p, p, p );
-    __m128i kBase128        = _mm_set_epi32( kBase, kBase, kBase, kBase );
-    __m128i kMod128         = _mm_set_epi32( kMod, kMod, kMod, kMod );
-    __m128i rollingHash128  = zeroes;
+    __m256i power256        = _mm256_set_epi32( p, p, p, p, p, p, p, p );
+    __m256i kBase256        = _mm256_set_epi32( kBase, kBase, kBase, kBase, kBase, kBase, kBase, kBase );
+    __m256i kMod256         = _mm256_set_epi32( kMod, kMod, kMod, kMod, kMod, kMod, kMod, kMod );
+    __m256i rollingHash256  = zeroes;
 
     for ( auto i = 0; i < slen; i++ )
     {
         // thash = ( thash * kBase + t[ i ] ) % kMod;
 
-        auto tadd       = input[ i ]; // _mm_load_si128( reinterpret_cast< __m128i *>( &input[ i * 4 ] ) );
+        auto tadd       = _mm256_set_epi32(
+            input[ streamSize * 0 + i ],
+            input[ streamSize * 1 + i ],
+            input[ streamSize * 2 + i ],
+            input[ streamSize * 3 + i ],
+            input[ streamSize * 4 + i ],
+            input[ streamSize * 5 + i ],
+            input[ streamSize * 6 + i ],
+            input[ streamSize * 7 + i ] );
 
-        auto a          = _mm_mullo_epi32( rollingHash128, kBase128 );
-        auto b          = _mm_add_epi32( a, tadd );
+        auto a          = _mm256_mul_epi32( rollingHash256, kBase256 );
+        auto b          = _mm256_add_epi32( a, tadd );
 
-        rollingHash128  = _mm_and_si128( b, modMask );
+        rollingHash256  = _mm256_and_si256( b, modMask );
     }
 
-    vector< int > hashes[ 4 ];
-
-    int index = 0; 
-    for ( auto i = slen; i < mapSize / 4; i++ )
+    for ( auto i = slen; i < ( MMAP_SIZE / streams ); i++ )
     {
         // if ( rollingHash == targetHash ) { }
 
-        auto result = _mm_cmpeq_epi32( rollingHash128, targetHash128 );
-        output[ i ] = result;
+        auto result         = _mm256_cmpeq_epi32( rollingHash256, targetHash128 );
+        output[ i ]         = result;
 
         // thash -= ( t[ i - s.size() ] * power ) % kMod;
 
         // printf( "%c", input[ 1 ] );
-        auto remove         = input[ i - 5 ]; // _mm_load_si128( reinterpret_cast< __m128i *>( &input[ ( i - 5 ) * 4 ] ) );
-        auto a              = _mm_mullo_epi32( remove, power128 );
-        auto b              = _mm_and_si128( a, modMask );
-        rollingHash128      = _mm_sub_epi32( rollingHash128, b );
+        auto remove         = _mm256_set_epi32(
+            input[ 512 * 0 + i - slen ],
+            input[ 512 * 1 + i - slen ],
+            input[ 512 * 2 + i - slen ],
+            input[ 512 * 3 + i - slen ],
+            input[ 512 * 4 + i - slen ],
+            input[ 512 * 5 + i - slen ],
+            input[ 512 * 6 + i - slen ],
+            input[ 512 * 7 + i - slen ] );
+
+        auto a              = _mm256_mullo_epi32( remove, power256 );
+        auto b              = _mm256_and_si256( a, modMask );
+        rollingHash256      = _mm256_sub_epi32( rollingHash256, b );
 
         // if ( thash < 0 ) thash += kMod;
 
-        auto cmp            = _mm_cmplt_epi32( rollingHash128, zeroes );
-        auto to_add         = _mm_and_si128( kMod128, cmp );
-        rollingHash128      = _mm_add_epi32( rollingHash128, to_add );
+        auto cmp            = _mm256_cmpgt_epi32( zeroes, rollingHash256 );
+        auto to_add         = _mm256_and_si256( kMod256, cmp );
+        rollingHash256      = _mm256_add_epi32( rollingHash256, to_add );
 
         // thash = ( thash * kBase + t[ i ] ) % kMod;
 
-        auto tadd           = input[ i ]; // _mm_load_si128( reinterpret_cast< __m128i *>( &input[ i * 4 ] ) );
+//        auto tadd           = input[ i ];
+        auto tadd       = _mm256_set_epi32(
+            input[ streamSize * 0 + i ], input[ streamSize * 1 + i ], input[ streamSize * 2 + i ], input[ streamSize * 3 + i ],
+            input[ streamSize * 4 + i ], input[ streamSize * 5 + i ], input[ streamSize * 6 + i ], input[ streamSize * 7 + i ] );
 
-        a                   = _mm_mullo_epi32( rollingHash128, kBase128 );
-        b                   = _mm_add_epi32( a, tadd );
+        a                   = _mm256_mullo_epi32( rollingHash256, kBase256 );
+        b                   = _mm256_add_epi32( a, tadd );
 
-        rollingHash128      = _mm_and_si128( b, modMask );
+        rollingHash256      = _mm256_and_si256( b, modMask );
     }
-
-//    vector< int > total_hashes;
-//    for ( auto k = 0; k < 4; k++ )
-//    {
-//        total_hashes.insert( total_hashes.end(), hashes[ k ].begin(), hashes[ k ].end() );
-//    }
-
-    for ( auto i = 0; i < 4; i++ )
-    {
-        for ( auto & hash : hashes[ i ] )
-        {
-//            printf( "0x%08x\n", hash );
-        }
-
-//        printf( "new set\n" );
-    }
-
-//    return result[ 0 ]
-//   + result [ 1 ]
-//   + result [ 2 ]
-//   + result [ 3 ]  ;
 }
 
 
 int main()
 {
-    int mapSize = 4096;
-    int fd      = open( "big.txt", O_RDONLY );
+    int fd      = open( "big.txt2", O_RDONLY );
     int size    = lseek( fd, 0, SEEK_END );
-    string s    = "woman";
+    string s    = "certificates";
+//    string s    = "woman";
 
     vector< Match > matches;
 
-    for ( auto k = 0; k < size; k += mapSize )
+    for ( auto k = 0; k < size; k += MMAP_SIZE )
     {
-        auto data = mmap( 0, 4096, PROT_READ, MAP_SHARED, fd, k );
+        auto data = mmap( 0, MMAP_SIZE, PROT_READ, MAP_SHARED, fd, k );
 
         char * t = ( char * ) data;
 
@@ -153,7 +152,7 @@ int main()
 
 #if 0
         int found = 0;
-        for ( auto i = s.size(); i < mapSize; i++ )
+        for ( auto i = s.size(); i < MMAP_SIZE; i++ )
         {
             if ( rollingHash == targetHash )
             {
@@ -177,38 +176,65 @@ int main()
         }
 #endif
 
-#if 1
-        __m128i remap[ mapSize ];
-        for ( auto i = 0; i < mapSize / 4; i++ )
+        int streams     = 8;
+        int streamSize  = MMAP_SIZE / streams;
+
+        int     remap[ MMAP_SIZE  ];
+        for ( auto i = 0; i < MMAP_SIZE; i++ )
         {
-            int *input = ( int * ) &remap[ i ];
-            input[ 0 ] = t[ 1024 * 0 + i ];
-            input[ 1 ] = t[ 1024 * 1 + i ];
-            input[ 2 ] = t[ 1024 * 2 + i ];
-            input[ 3 ] = t[ 1024 * 3 + i ];
+//            int *input = ( int * ) &remap[ i ];
+//
+//            for ( auto j = 0; j < streams; j++ )
+//            {
+//                input[ j ] = t[ streamSize * j + i ];
+//            }
+            remap[ i ] = t[ i ];
+        }
+
+        __m256i output[ MMAP_SIZE / streams ];
+        vsearch( streams, remap, output, targetHash, s.size(), power );
+
+#if 0
+        int out[ MMAP_SIZE ];
+        for ( auto i = 0; i < MMAP_SIZE / streams; i++ )
+        {
+            int *o = ( int * ) &output[ i ];
+
+            for ( auto j = 0; j < streams; j++ )
+            {
+                out[ streamSize * j + i ] = o[ j ];
+            }
         }
 #endif
 
-        char thash_arr[ 4 ] = { 0 };
-//        int output[ mapSize ];
-        __m128i output[ 1024 ];
-        vsearch( remap, output, targetHash, s.size(), power );
+//        for ( auto i = 0; i < 1024; i++ )
+//        {
+//            count += ( ( int * ) &output[ i ] )[ 0 ] & 1;
+//            count += ( ( int * ) &output[ i ] )[ 1 ] & 1;
+//            count += ( ( int * ) &output[ i ] )[ 2 ] & 1;
+//            count += ( ( int * ) &output[ i ] )[ 3 ] & 1;
+//        }
 
-//        auto temp = output[ 0 ];
-
-        for ( auto i = 0; i < 1024; i++ )
+#if 0
+//        count = 0;
+        for ( auto i = 0; i < 4096; i++ )
         {
-            count += ( ( int * ) &output[ i ] )[ 0 ] & 1;
-            count += ( ( int * ) &output[ i ] )[ 1 ] & 1;
-            count += ( ( int * ) &output[ i ] )[ 2 ] & 1;
-            count += ( ( int * ) &output[ i ] )[ 3 ] & 1;
+            if ( out[ i ] )
+            {
+                if ( memcmp( ( char * )&t[ i - s.size() ], s.c_str(), s.size() ) == 0 )
+                {
+                    count++;
+                }
+            }
         }
+#endif
 
-//        printf( "%c", thash_arr[ 2 ] );
+        munmap( data, MMAP_SIZE );
     }
 
     cout << "Matches = " << matches.size() << endl;
     cout << "Count = " << count << endl;
+    cout << "Line Count = " << lineCount << endl;
 
     return 0;
 }
