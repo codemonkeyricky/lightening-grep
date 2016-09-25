880b4d1815dc66fa994891c22b112c327537fced
Ricky <codemonkeyricky@gmail.com>
2016-09-25 10:40:45 -0700

    Clean up.

--- SearcherSSE.cpp
+++ SearcherSSE.cpp
@@ -36,60 +36,51 @@ uint32_t SearcherSSE::process(
     int size    = lseek( fd, 0, SEEK_END );
     string s    = pattern;
 
+    // Limit for now.
+    assert( pattern.size() <= 32 );
+
     int count = 0;
+
+    alignas( 32 ) char first8bitsRepeated[ 32 ];
+    memset( first8bitsRepeated, pattern[ 0 ], 32 );
+    auto firstLetterRepated = _mm256_loadu_si256( ( const __m256i * ) &first8bitsRepeated[ 0 ] );
+
+    alignas( 32 ) char stridedPattern[ 32 ][ 32 ];
+    __m256i patternStrided256[ 32 ];
+
+    for ( auto i = 0; i < 32; i++ )
+    {
+        memset( stridedPattern[ i ], 0, 32 );
+
+        memcpy( &stridedPattern[ i ][ i ], pattern.c_str(), 32 - i );
+
+        patternStrided256[ i ] = _mm256_loadu_si256( ( const __m256i * ) &stridedPattern[ i ] );
+    }
 
     for ( auto k = 0; k < size; k += MMAP_SIZE )
     {
-        auto data = mmap( 0, MMAP_SIZE, PROT_READ, MAP_SHARED, fd, k );
+        char * memoryMap = ( char * ) mmap( 0, MMAP_SIZE, PROT_READ, MAP_SHARED, fd, k );
 
-        char * t = ( char * ) data;
+//        // NOTE: _SIDD_LEAST_SIGNIFICANT sets the same bit as _SIDD_BIT_MASK
+//        // const int mode = _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_EACH | _SIDD_UNIT_MASK;
+//        const int mode = _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_EACH;
 
-        __m128i input, b;
-
-        // NOTE: _SIDD_LEAST_SIGNIFICANT sets the same bit as _SIDD_BIT_MASK
-        // const int mode = _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_EACH | _SIDD_UNIT_MASK;
-        const int mode = _SIDD_UBYTE_OPS | _SIDD_CMP_EQUAL_EACH;
-
-        alignas( 32 ) char first8bitsRepeated[ 16 ];
-        memset( first8bitsRepeated, pattern[ 0 ], 16 );
-        auto first = _mm_loadu_si128( ( const __m128i * ) &first8bitsRepeated[ 0 ] );
-
-        alignas( 32 ) char stridedPattern[ 16 ][ 16 ];
-        __m128i stridedPattern128[ 16 ];
-        for ( auto i = 0; i < 16; i++ )
+        for ( auto i = 0; i < MMAP_SIZE; i += 32 )
         {
-            memset( stridedPattern[ i ], 0, 16 );
+            auto octadword  = _mm256_load_si256( ( const __m256i * ) ( memoryMap + i ) );
 
-            memcpy( &stridedPattern[ i ][ i ], pattern.c_str(), 16 - i );
+            auto result     = _mm256_cmpeq_epi8( octadword, firstLetterRepated );
+            auto resultMask = _mm256_movemask_epi8( result );
 
-            stridedPattern128[ i ] = _mm_loadu_si128( ( const __m128i * ) &stridedPattern[ i ] );
-        }
-
-        auto i = 0;
-//        while ( i < MMAP_SIZE )
-        for ( auto i = 0; i < 4096; i += 16 )
-        {
-            input = _mm_load_si128( ( const __m128i * ) ( t + i ) );
-
-            auto result = _mm_cmpeq_epi8( input, first );
-            auto mask = _mm_movemask_epi8( result );
-
-            if ( mask )
+            if ( resultMask )
             {
-                int pos = 0;
-                auto m = mask;
-                while ( m )
-                {
-                    m >>= 1;
+                unsigned int m  = resultMask;
+                int lsbPos      = ffs( m ) - 1;
+                auto r          = _mm256_cmpeq_epi8( octadword, patternStrided256[ lsbPos ] );
+                auto m2         = _mm256_movemask_epi8( r );
+                auto c          = _mm_popcnt_u32( m2 );
 
-                    pos++;
-                }
-                --pos;
-
-                auto r = _mm_cmpeq_epi8( input, stridedPattern128[ pos ] );
-                auto m2 = _mm_movemask_epi8( r );
-                auto c = _mm_popcnt_u32( m2 );
-                if ( c == ( 16 - pos ) )
+                if ( c == ( 32 - lsbPos ) )
                 {
                     if ( c > 5 )
                         count++;
@@ -97,7 +88,7 @@ uint32_t SearcherSSE::process(
             }
         }
 
-        munmap( data, MMAP_SIZE );
+        munmap( memoryMap, MMAP_SIZE );
     }
 
     return count;
