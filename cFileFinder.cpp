75f75285b2012826682f4bd4b3fdd8346978db48
code.monkey.ricky@gmail.com <codemonkeyricky@gmail.com>
2016-11-01 06:46:45 -0700

    Preliminary file filter support.

--- cFileFinder.cpp
+++ cFileFinder.cpp
@@ -2,10 +2,85 @@
 #include <string.h>
 
 #include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <set>
 
 #include "cFileFinder.hpp"
 
 using namespace std;
+
+std::map< std::string, std::set< std::string > > f_filters =
+{
+   { "actionscript",    { ".as", ".mxml" } },
+   { "ada",             { ".ada", ".adb", ".ads" } },
+   { "asm",             { ".asm", ".s" } },
+   { "asp",             { ".asp" } },
+   { "aspx",            { ".master", ".ascx", ".asmx", ".aspx", ".svc" } },
+   { "batch",           { ".bat", ".cmd" } },
+   { "cc",              { ".c", ".h", ".xs" } },
+
+//   {"cfmx        ", { ".cfc",  .cfm .cfml
+//   {"clojure     ", { ".clj",
+//   {"cmake       ", { "CMakeLists.txt; .cmake
+//   {"coffeescript", { ".coffee
+//   {"cpp         ", { ".cpp .cc .cxx .m .hpp .hh .h .hxx
+//   {"csharp      ", { ".cs
+//   {"css         ", { ".css
+//   {"dart        ", { ".dart
+//   {"delphi      ", { ".pas .int .dfm .nfm .dof .dpk .dproj .groupproj .bdsgroup .bdsproj
+//   {"elisp       ", { ".el
+//   {"elixir      ", { ".ex .exs
+//   {"erlang      ", { ".erl .hrl
+//   {"fortran     ", { ".f .f77 .f90 .f95 .f03 .for .ftn .fpp
+//   {"go          ", { ".go
+//   {"groovy      ", { ".groovy .gtmpl .gpp .grunit .gradle
+//   {"haskell     ", { ".hs .lhs
+//   {"hh          ", { ".h
+//   {"html        ", { ".htm .html
+//   {"java        ", { ".java .properties
+//   {"js          ", { ".js
+//   {"json        ", { ".json
+//   {"jsp         ", { ".jsp .jspx .jhtm .jhtml
+//   {"less        ", { ".less
+//   {"lisp        ", { ".lisp .lsp
+//   {"lua         ", { ".lua; first line matches /^#!.*\blua(jit)?/
+//   {"make        ", { ".mk; .mak; makefile; Makefile; GNUmakefile
+//   {"matlab      ", { ".m
+//   {"md          ", { ".mkd; .md
+//   {"objc        ", { ".m .h
+//   {"objcpp      ", { ".mm .h
+//   {"ocaml       ", { ".ml .mli
+//   {"parrot      ", { ".pir .pasm .pmc .ops .pod .pg .tg
+//   {"perl        ", { ".pl .pm .pod .t .psgi; first line matches /^#!.*\bperl/
+//   {"perltest    ", { ".t
+//   {"php         ", { ".php .phpt .php3 .php4 .php5 .phtml; first line matches /^#!.*\bphp/
+//   {"plone       ", { ".pt .cpt .metadata .cpy .py
+//   {"pmc         ", { ".pmc
+//   {"python      ", { ".py; first line matches /^#!.*\bpython/
+//   {"rake        ", { "Rakefile
+//   {"rr          ", { ".R
+//   {"ruby        ", { ".rb .rhtml .rjs .rxml .erb .rake .spec; Rakefile; first line matches /^#!.*\bruby/
+//   {"rust        ", { ".rs
+//   {"sass        ", { ".sass .scss
+//   {"scala       ", { ".scala
+//   {"scheme      ", { ".scm .ss
+//   {"shell       ", { ".sh .bash .csh .tcsh .ksh .zsh .fish; first line matches /^#!.*\b(?:ba|t?c|k|z|fi)?sh\b/
+//   {"smalltalk   ", { ".st
+//   {"sql         ", { ".sql .ctl
+//   {"tcl         ", { ".tcl .itcl .itk
+//   {"tex         ", { ".tex .cls .sty
+//   {"textile     ", { ".textile
+//   {"tt          ", { ".tt .tt2 .ttml
+//   {"vb          ", { ".bas .cls .frm .ctl .vb .resx
+//   {"verilog     ", { ".v .vh .sv
+//   {"vhdl        ", { ".vhd .vhdl
+//   {"vim         ", { ".vim
+//   {"xml         ", { ".xml .dtd .xsl .xslt .ent; first line matches /<[?]xml/
+//   {"yaml        ", { ".yaml .yml
+};
+
 
 void cFileFinder::exploreDirectory(
     int                     workerThreads,
@@ -13,6 +88,8 @@ void cFileFinder::exploreDirectory(
     iQueue< sSearchEntry >  *list
     )
 {
+    auto filter = f_filters[ "cc" ];
+
     std::queue< std::string >   toExplore;
 
     toExplore.push( root );
