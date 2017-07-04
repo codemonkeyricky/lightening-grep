29725cb98292025b7ecf7d2e412a59b4a3f75dab
Richard Tang <code.monkey.ricky@gmail.com>
2017-07-03 17:22:06 -0700

    Attempted to enable > 16 char support. < 16 char working again.

--- ff.cpp
+++ ff.cpp
@@ -15,15 +15,15 @@ int LevenshteinDistanceScalar(
 {
     std::vector< std::vector< int > > mat( A.size() + 1, std::vector< int >( B.size() + 1 ) ); 
 
-    for ( auto j = 0; j <= B.size(); j++ ) 
+    for ( auto j = 0u; j <= B.size(); j++ ) 
         mat[ 0 ][ j ] = j; 
 
-    for ( auto j = 0; j <= A.size(); j++ ) 
+    for ( auto j = 0u; j <= A.size(); j++ ) 
         mat[ j ][ 0 ] = j; 
 
-    for ( auto i = 1; i <= A.size(); i++ )
+    for ( auto i = 1u; i <= A.size(); i++ )
     {
-        for ( auto j = 1; j <= B.size(); j++ )
+        for ( auto j = 1u; j <= B.size(); j++ )
         {
             if ( A[ i - 1 ] == B[ j - 1 ] ) 
             {
@@ -49,68 +49,110 @@ int LevenshteinDistance(
     std::string candidate
     )
 {
+#define MAX_CHAR        32
+#define CHAR_PER_REG    16
+
     std::vector< uint8_t > initial = 
     { 
-        0, 1, 2, 3, 4, 5, 6, 7,
-        8, 9, 10, 11, 12, 13, 14, 15
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
     }; 
 
-    std::vector< uint8_t > t; 
-    t.resize( 16 ); 
+    std::vector< uint8_t > t = initial;
+    t.resize( MAX_CHAR + 1 ); 
+    std::vector< uint8_t > ts; 
+    std::copy( t.begin() + 1, t.end(), std::back_inserter( ts ) ); 
+    ts.resize( MAX_CHAR + 1 ); 
 
     auto targetLen = target.length();
     auto candidateLen = candidate.length();
-    target.resize( 16 ); 
-    __m128i c   = _mm_load_si128( ( const __m128i * ) &initial[ 0 ] );
-    __m128i ones= _mm_set1_epi8( 1 ); 
-    __m128i s   = _mm_load_si128( ( const __m128i * ) &target[ 0 ] ); 
-    __m128i e, a; 
-    for ( auto i = 0; i < candidateLen; i ++ )
+    auto copy = target; 
+    copy.resize( MAX_CHAR + 1 ); 
+
+    // Calculate number of xmm registers required.
+    auto regReq = targetLen / CHAR_PER_REG;
+    regReq += targetLen % CHAR_PER_REG ? 1 : 0;
+
+    // Initialize current and current shifted.
+    __m128i c[ regReq ], cs[ regReq ];
+    for ( auto i = 0u; i < regReq; i++ )
     {
-        // edit remains.
-        e       = c; 
+        c[ i ]  = _mm_load_si128( ( const __m128i * ) &( t[ i * CHAR_PER_REG ] ) );
+    }
 
-        // shift add to align with edit. 
-        a       = _mm_srli_si128( c, 1 );
+    // Initialize ones. 
+    auto o = _mm_set1_epi8( 1 ); 
 
-        // Only need to substitute if character is different.
-        auto r  = _mm_set1_epi8( candidate[ i ] );
+    // Initialize source variable.
+    __m128i s[ regReq ];
+    for ( auto i = 0u; i < regReq; i++ )
+    {
+        s[ i ] = _mm_load_si128( ( const __m128i * ) &( copy[ i * CHAR_PER_REG ] ) ); 
+    }
 
-        // m = compare entire word with current letter
-        // e = m * e + !m * ( min( a, e ) + 1 ); 
+    // Create edit and add arrays.
+    __m128i e[ regReq ], a[ regReq ]; 
 
+    for ( auto i = 0u; i < candidateLen; i ++ )
+    {
         // 
-        // Find the chars that just carries over -> p1
+        // Prepare the shifted variable for next iteration.
         // 
 
-        auto m  = _mm_cmpeq_epi8( s, r );
-        auto p1 = _mm_and_si128( m, e ); 
+        for ( auto j = 0u; j < regReq; j++ )
+        {
+            c[ j ]  = _mm_load_si128( ( const __m128i * ) &t[ j * CHAR_PER_REG ] );
+            cs[ j ] = _mm_load_si128( ( const __m128i * ) &ts[ j * CHAR_PER_REG ] );
+        }
 
-        // 
-        // Find the minimum between edits and adds, then +1 -> p2 
-        // 
+        for ( auto j = 0u; j < regReq; j++ )
+        {
+            // edit just mirrorss current.
+            e[ j ]  = c[ j ]; 
 
-        auto min    = _mm_min_epi8( a, e ); 
-        min         = _mm_add_epi8( min, ones );
-        auto nm     = ~m; 
-        auto p2     = _mm_and_si128( nm, min ); 
+            // add needs to be shifted. 
+            a[ j ]  = cs[ j ];
 
-        auto sum    = _mm_add_epi8( p1, p2 ); 
-        sum         = _mm_slli_si128( sum, 1 );
+            // Only need to substitute if character is different.
+            auto r  = _mm_set1_epi8( candidate[ i ] );
+
+            // m = compare entire word with current letter
+            // e = m * e + !m * ( min( a, e ) + 1 ); 
+
+            // 
+            // Find the chars that just carries over -> p1
+            // 
+
+            auto m  = _mm_cmpeq_epi8( s[ j ], r );
+            auto p1 = _mm_and_si128( m, e[ j ] ); 
+
+            // 
+            // Find the minimum between edits and adds, then +1 -> p2 
+            // 
+
+            auto min    = _mm_min_epi8( a[ j ], e[ j ] ); 
+            min         = _mm_add_epi8( min, o );
+            auto nm     = ~m; 
+            auto p2     = _mm_and_si128( nm, min ); 
+
+            auto sum    = _mm_add_epi8( p1, p2 ); 
+            sum         = _mm_slli_si128( sum, 1 );
+
+            _mm_store_si128( ( __m128i * ) &t[ j * CHAR_PER_REG ], sum ); 
+        }
 
         // 
         // Now account for deletes -> c 
         // 
 
-        _mm_store_si128( ( __m128i * ) &t[ 0 ], sum ); 
-
         t[ 0 ] = i + 1; 
-        for ( auto j = 1; j < ( targetLen + 1 ); j ++ )
+        for ( auto j = 1u; j < ( targetLen + 1 ); j ++ )
         {
             t[ j ] = std::min( (uint8_t) ( t[ j - 1 ] + 1 ), t[ j ] ); 
         }
 
-        c  = _mm_load_si128( ( const __m128i * ) &t[ 0 ] );
+        ts.clear();
+        std::copy( t.begin() + 1, t.end(), std::back_inserter( ts ) ); 
     }
 
     return t[ targetLen ]; 
@@ -180,12 +222,16 @@ int main(
     for ( auto &p : list ) 
     {
 #endif 
+#if 0
         std::string t( "source.c" ); 
         std::string p( "sourc" ); 
+        #endif
+        std::string t( "bread" ); 
+        std::string p( "fred" ); 
 
         auto t1 = std::chrono::high_resolution_clock::now(); 
 
-        for ( auto i = 0; i < 10000; i ++ )
+        for ( auto i = 0; i < 1; i ++ )
         {
             volatile int d1 = LevenshteinDistance( t, p );
             volatile int d0 = 0;
@@ -193,7 +239,7 @@ int main(
 
         auto t2 = std::chrono::high_resolution_clock::now(); 
 
-        for ( auto i = 0; i < 10000; i ++ )
+        for ( auto i = 0; i < 1; i ++ )
         {
             volatile int d2 = LevenshteinDistanceScalar( t, p ); 
             volatile int d0 = 0;
